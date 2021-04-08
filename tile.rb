b3b9ffe1bb7a9a0061f6ade5760e3912d8864bbb
Josh Meyers <pianoman04428@gmail.com>
2021-04-08 16:50:01 -0500

    nothing

--- tile.rb
+++ tile.rb
@@ -19,16 +19,14 @@ class Tile
   def initialize(board, pos)
     @board, @pos = board, pos
 
-    @revealed, @bomb, @flagged = false, false, false
+    @explored, @bomb, @flagged = false, false, false
   end
-
-
 
   def inspect
-    {"pos" => @pos, "revealed"=> @revealed, "bomb?"=> @bomb, "flagged?"=> @flagged}
+    {"pos" => @pos, "explored?"=> @explored, "bomb?"=> @bomb, "flagged?"=> @flagged}
   end
 
-  def get_neighbors
+  def neighbors
     adjacents = DELTAS.map do |(dx, dy)|
       [pos[0] + dx, pos[1] + dy]
     end.select do |row, col|
@@ -39,27 +37,34 @@ class Tile
     adjacents.map { |pos| @board[pos] }
   end
 
+
+  def adjacent_bomb_count
+    neighbors.count{ |neighbor| neighbor.bomb }
+  end
+
+  def explore
+
+  end
+
   def plant_bomb
     @bomb = true
   end
 
-  # def ==(other_tile)
-  #   other_tile.is_a?(Tile) &&
-  #     other_tile.pos == self.pos
-  # end
-
   def flag
-    flagged = !flagged unless revealed
+    flagged = !flagged unless explored
   end
-
+  
+  def render
+    if flagged
+      "F"
+    elsif explored
+      adjacent_bomb_count == 0 ? "_" : adjacent_bomb_count.to_s
+    else
+      "*"
+    end
+  end
+  
   def reveal
-    @revealed = true
+  ### called at the end of the game to show the board in its actual state
   end
-
-  def to_s
-
-  end
-
-  private
-
 end
