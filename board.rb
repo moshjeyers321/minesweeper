3f1e4a922d121109ffb5012e93b2b976c0d42d00
Josh Meyers <pianoman04428@gmail.com>
2021-04-08 15:17:03 -0500

    fixed board and tile so now we actually can create a board with randomly placed bombs

--- board.rb
+++ board.rb
@@ -1,8 +1,34 @@
+require_relative "tile"
+
 class Board
 
+  attr_reader :board_size, :num_bombs
+  
+  def initialize(board_size, num_bombs)
+    @board_size, @num_bombs = board_size, num_bombs
 
-  def initialize
+    generate_board
+  end
 
+  def generate_board
+    @board = Array.new(@board_size) do |row|
+      Array.new(@board_size) { |col| Tile.new(self, [row, col]) }
+    end
+
+    plant_bombs
+  end
+
+  def plant_bombs
+    total_bombs = 0
+    while total_bombs < @num_bombs
+      rand_pos = Array.new(2) { rand(@board_size) }
+
+      tile = self[rand_pos]
+      next if tile.bomb
+
+      tile.plant_bomb
+      total_bombs += 1
+    end
   end
 
   def [](pos)
--- tile.rb
+++ tile.rb
@@ -1,49 +1,37 @@
 
 
 class Tile
+  
+  attr_reader :board, :bomb
+  attr_accessor :flagged, :pos
 
-  attr_reader :board
+  def initialize(board, pos)
+    @board, @pos = board, pos
 
-  def initialize(board)
-    @board = board
-    @board_size = board.length
-    @revealed = false
-    @bomb = false
-    @flagged = false
-
+    @revealed, @bomb, @flagged = false, false, false
   end
 
 
 
   def inspect
-    { "revealed"=> @revealed, "bomb?"=> @bomb, "flagged?"=> @flagged, "pos"=> self.pos}
+    {"pos" => @pos, "revealed"=> @revealed, "bomb?"=> @bomb, "flagged?"=> @flagged}
   end
 
   def neighbors
 
   end
 
-  def pos
-    pos = nil
-    board.each.with_index do |row, xi|
-      row.each.with_index do |col, yi|
-        pos = [xi, yi] if self == board[xi,yi]
-      end
-    end
-    pos
+  def plant_bomb
+    @bomb = true
   end
 
-  def ==(other_tile)
-    other_tile.is_a?(Tile) &&
-      other_tile.pos == self.pos
-  end
+  # def ==(other_tile)
+  #   other_tile.is_a?(Tile) &&
+  #     other_tile.pos == self.pos
+  # end
 
   def flag
-    if !flagged
-      flagged = true
-    else
-      flagged = false
-    end
+    flagged = !flagged unless revealed
   end
 
   def reveal
@@ -56,5 +44,4 @@ class Tile
 
   private
 
-  attr_accessor :flagged
 end
